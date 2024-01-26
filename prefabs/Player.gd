d494f060b1051610440ed72287c740fde0a93570
Dorian Kaczmarek <itsmedek@proton.me>
2024-01-26 22:52:46 +0100

    Add room_area1

--- prefabs/Player.gd
+++ prefabs/Player.gd
@@ -1,29 +1,42 @@
 extends StaticBody2D
 
-@export var MOVEMENT_SPEED: float = 200.0
+@export var MOVEMENT_SPEED: float = 180.0
 
 func _ready():
 	$AnimatedSprite2D.animation = "move_south"
 
 func _process(delta):
 	var moving: bool = false
+	var animation_playing: bool = false
 	
-	if Input.is_action_pressed("move_north"):
-		self.position.y += -1.0 * delta * MOVEMENT_SPEED
-		$AnimatedSprite2D.play("move_north")
-		moving = true
-	elif Input.is_action_pressed("move_south"):
-		self.position.y += 1.0 * delta * MOVEMENT_SPEED
-		$AnimatedSprite2D.play("move_south")
-		moving = true
-	elif Input.is_action_pressed("move_west"):
+	if Input.is_action_pressed("move_west"):
+		if !animation_playing:
+			$AnimatedSprite2D.play("move_west")
 		self.position.x += -1.0 * delta * MOVEMENT_SPEED
-		$AnimatedSprite2D.play("move_west")
+		
 		moving = true
-	elif Input.is_action_pressed("move_east"):
+		animation_playing = true
+	if Input.is_action_pressed("move_east"):
+		if !animation_playing:
+			$AnimatedSprite2D.play("move_east")
 		self.position.x += 1.0 * delta * MOVEMENT_SPEED
-		$AnimatedSprite2D.play("move_east")
+		
 		moving = true
+		animation_playing = true
+	if Input.is_action_pressed("move_north"):
+		if !animation_playing:
+			$AnimatedSprite2D.play("move_north")
+		self.position.y += -1.0 * delta * MOVEMENT_SPEED
+		
+		moving = true
+		animation_playing = true
+	if Input.is_action_pressed("move_south"):
+		if !animation_playing:
+			$AnimatedSprite2D.play("move_south")
+		self.position.y += 1.0 * delta * MOVEMENT_SPEED
+		
+		moving = true
+		animation_playing = true
 	
 	if !moving:
 		$AnimatedSprite2D.stop()
